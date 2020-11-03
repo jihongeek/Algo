0fa542ce90b7f22c04f8f2379c107c2ee12551e7
jihongeek <jihongeek@kakao.com>
2020-11-04 01:05:18 +0900

    DNA done

--- baekjoon/1969.cpp
+++ baekjoon/1969.cpp
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-char tmp[51];
+int nuke[] = {'A','C','G','T'};
+int nuke_nums[] = {0,0,0,0};
+char answer_dna[51];
 char dna[1000][51];
-int HDsum[1000];
-int inttmp;
+int HDsum;
+int max_n,max;
+
 int main(void)
 {
     int n,m;
@@ -15,52 +17,42 @@ int main(void)
     {
         scanf("%s",dna[i]);        
     }
-    for (int i = 0; i < n; i++)
+    
+    for (int i = 0; i < m; i++)
     {
+        max_n = 0;
+        max = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            nuke_nums[j] = 0;
+        }
         for (int j = 0; j < n; j++)
         {
-            for (int k = 0; k < m; k++)
-            {
-                if (i == j) continue;
-                if (dna[i][k] != dna[j][k])
-                {
-                    HDsum[i]++;
-                }
-            }
+            if (dna[j][i] == 'A') nuke_nums[0]++;               
+            if (dna[j][i] == 'C') nuke_nums[1]++;
+            if (dna[j][i] == 'G') nuke_nums[2]++;               
+            if (dna[j][i] == 'T') nuke_nums[3]++;               
         }    
-    }
+        for (int j = 0; j < 4; j++)
+        {
+            if (nuke_nums[j] > max)
+            {
+                max_n = nuke[j];
+                max = nuke_nums[j];
+            }
+        }
+        answer_dna[i] = max_n;    
 
+    }
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - 1; j++)
+        for (int j = 0; j < m; j++)
         {
-            if (HDsum[j] > HDsum[j+1])
+            if (answer_dna[j] != dna[i][j])
             {
-                strcpy(tmp, dna[j]);
-                strcpy(dna[j],dna[j+1]);
-                strcpy(dna[j+1],tmp);
-                
-                inttmp = HDsum[j];
-                HDsum[j] = HDsum[j+1];
-                HDsum[j+1] = inttmp; 
+                HDsum++;
             }
-            else if (HDsum[j] == HDsum[j+1])
-            {
-                if (strcmp(dna[j],dna[j+1]) == -1)
-                {
-                    strcpy(tmp, dna[j]);
-                    strcpy(dna[j], dna[j + 1]);
-                    
-                    strcpy(dna[j + 1], tmp);
-                    
-                    inttmp = HDsum[j];
-                    HDsum[j] = HDsum[j + 1];
-                    HDsum[j + 1] = inttmp;
-                }
-            }
-
         }
     }
-    printf("%d\n%s",HDsum[0],dna[0]);    
-    
+    printf("%s\n%d",answer_dna,HDsum);    
 }
