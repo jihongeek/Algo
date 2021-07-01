3d95bd2ef6e026d6da35fdeb298bedfd9094c562
jihongeek <jihongeek@kakao.com>
2021-07-01 10:49:33 +0900

    backtracking : avoid print samecase by using set and frozenset

--- baekjoon/15650.py
+++ baekjoon/15650.py
@@ -1,14 +1,19 @@
 from sys import stdin 
 
 n,m = map(int,stdin.readline().strip().split())
-
-def permuFunc(start, n, c):
+found = set([])
+def permuFunc(n, c, choosed : list):
     if c == 0:
-        print()
+        if frozenset(choosed) not in found: 
+            for i in choosed:
+                print(f"{i}", end=" ")
+            found.add(frozenset(choosed))
+            print()
         return
-    print(start,end=" ")
-    for i in range(start+1,n+1):
-        permuFunc(i,n,c-1)
+    for i in range(choosed[-1]+1,n+1):
+        permuFunc(n,c-1,choosed + [i])
 
-permuFunc(1,n,m)
+for i in range(1,n+1):
+    permuFunc(n,m-1,[i])
+
     
