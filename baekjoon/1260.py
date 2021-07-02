e0c59c6aa08b535e1935e36eb8c1aa8ec95a1aa0
jihongeek <jihongeek@kakao.com>
2021-07-02 12:44:51 +0900

    <wrong answer> graphsearch : bfs with queue and dfs with recursive func

--- baekjoon/1260.py
+++ baekjoon/1260.py
@@ -1,27 +1,45 @@
 from sys import stdin 
 from collections import deque
-def search(v,stype):
+def bfs(v):
     global n
     visited = [False]*n
     deq = deque()
     visited[v-1] = True
     deq.append(v-1)
     while len(deq) > 0:
-        now = deq.popleft() if stype == "bfs" else deq.pop() 
-        print(now+1,end =" ")
+        now = deq.popleft()
         for i in range(n):
             if graph[now][i] and not visited[i]:
                 visited[i] = True
+                print(now+1,end = " ")
                 deq.append(i)
-    print()
+def dfs(v,visited : list):
+    global n
+    visited[v-1] = True
+    print(v, end= " ")
+    isEnd = False
+    for i in range(n):
+        if graph[v-1][i] and not visited[i]:
+            visited[i] = True
+            dfs(i+1,visited) 
+            break 
+        else:
+            isEnd = True
+    if isEnd:
+        return
+
+    
+    
             
 n,m,v = map(int, stdin.readline().strip().split()) 
 graph = [[False]*n for _ in range(n)]
 for i in range(m):
     row,col = map(int, stdin.readline().strip().split())
     graph[row-1][col-1] = True
-search(v,"dfs")
-search(v,"bfs")
+    graph[col-1][row-1] = True
+dfs(v, [False]*n)
+print()
+bfs(v)
 
     
 
