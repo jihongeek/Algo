86471ecbe5848aa9c5dd035de087cc2bb23d1fd2
jihongeek <jihongeek@kakao.com>
2021-07-09 22:59:13 +0900

    sort : sort and calculate  sum of lines (but except same(overlaped) lines)

--- baekjoon/2170.py
+++ baekjoon/2170.py
@@ -5,7 +5,8 @@ lines.sort()
 sumOfLines = lines[0][1] - lines[0][0]  
 for i in range(1,n):
     if lines[i][0] < lines[i-1][1]:
-        if lines[i][1] < lines[i-1][1]:
+        if lines[i][1] < lines[i-1][1]: 
+            lines[i] = (lines[i][0],lines[i-1][1])
             continue
         sumOfLines += abs(lines[i][1] - lines[i-1][1])
     else:
