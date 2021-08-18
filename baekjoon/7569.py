d5956739a551f0d390820b8d6c111c553cf2c9c6
jihongeek <jihongeek@kakao.com>
2021-08-18 14:06:22 +0900

    bfs : find day to ripen all tomatos with bfs

--- baekjoon/7569.py
+++ baekjoon/7569.py
@@ -1,47 +1,64 @@
+from sys import stdin 
 from collections import deque
-import sys
 
 def bfs():
-    dx = [-1,1,0,0,0,0]
-    dy = [0,0,1,-1,0,0]
-    dz = [0,0,0,0,1,-1]
-    day = -1
-    while len(q) != 0:
-        z, y, x = q.popleft()
-        for i in range(6):
-            now_x = x + dx[i]
-            now_y = y + dy[i]
-            now_z = z + dz[i]
-            if now_x < m and now_x >= 0 and now_y < n and now_y >= 0 and now_z < h and now_z >= 0:
-                if box[now_z][now_y][now_x] == -1:
-                    continue
-                if (not visited[now_z][now_y][now_x]) and box[now_z][now_y][now_x] == 0:
-                    visited[now_z][now_y][now_x] = True
-                    box[now_z][now_y][now_x] = box[z][y][x] + 1
-                    q.append((now_z, now_y, now_x))
-    day =  box[z][y][x] - 1
+    global unripeTomato,maxDay,q
+    unripeToRipe = -len(q)
+    day = 0
+    while len(q) > 0:
+        location = q.popleft()
+        layerNum,rowNum,colNum,dayOfRipeness = location
+        box[layerNum][rowNum][colNum] = 1
+        unripeToRipe += 1
+        day = max(day,dayOfRipeness)
+        if layerNum - 1 >= 0 and not visited[layerNum-1][rowNum][colNum] and box[layerNum-1][rowNum][colNum] == 0:
+            visited[layerNum-1][rowNum][colNum] = True
+            q.append((layerNum-1,rowNum,colNum,dayOfRipeness+1))
+        if layerNum + 1 < h and not visited[layerNum+1][rowNum][colNum] and box[layerNum+1][rowNum][colNum] == 0:
+            visited[layerNum+1][rowNum][colNum] = True
+            q.append((layerNum+1,rowNum,colNum,dayOfRipeness+1))
+        if rowNum - 1 >= 0 and not visited[layerNum][rowNum-1][colNum] and box[layerNum][rowNum-1][colNum] == 0:
+            visited[layerNum][rowNum-1][colNum] = True
+            q.append((layerNum,rowNum-1,colNum,dayOfRipeness+1))
+        if rowNum + 1 < n and not visited[layerNum][rowNum+1][colNum] and box[layerNum][rowNum+1][colNum] == 0:
+            visited[layerNum][rowNum+1][colNum] = True
+            q.append((layerNum,rowNum+1,colNum,dayOfRipeness+1))
+        if colNum - 1 >= 0 and not visited[layerNum][rowNum][colNum-1] and box[layerNum][rowNum][colNum-1] == 0:
+            visited[layerNum][rowNum][colNum-1] = True
+            q.append((layerNum,rowNum,colNum-1,dayOfRipeness+1))
+        if colNum + 1 < m and not visited[layerNum][rowNum][colNum+1] and box[layerNum][rowNum][colNum+1] == 0:
+            visited[layerNum][rowNum][colNum+1] = True
+            q.append((layerNum,rowNum,colNum+1,dayOfRipeness+1))
     
-    for i in range(h):
-        for j in range(n):
-            for k in range(m):
-                if box[i][j][k] == 0:
-                    day = -1
-                    break
-    return day
+    maxDay = max(maxDay,day)
+    unripeTomato -= unripeToRipe
 
+m,n,h = map(int,stdin.readline().strip().split())
+unripeTomato = 0
+maxDay = 0
+box = []
+for layerNum in range(h):
+    board = []
+    for rowNum in range(n):
+        row = stdin.readline().strip().split()
+        for colNum in range(m):
+            row[colNum] = int(row[colNum])
+            if row[colNum] == 0:
+                unripeTomato += 1 
+        board.append(row)
+    box.append(board)
+
+visited = [[[False]*m for j in range(n)] for i in range(h)]
 q = deque()
 
-m, n, h = tuple(map(int,input().split()))
-
-visited = [ [ [False]*m for x in range(n) ] for floor in range(h) ]
-
-box = [ [ list(map(int,sys.stdin.readline().split())) for x in range(n) ] for floor in range(h) ]
-
-for i in range(h):
-    for j in range(n):
-        for k in range(m):
-            if (not visited[i][j][k]) and box[i][j][k] == 1:
-                visited[i][j][k] = True
-                q.append((i,j,k))           
-ans = bfs()
-print(ans)
+for layer in range(h):
+    for row in range(n):
+        for col in range(m):
+            if not visited[layer][row][col] and box[layer][row][col] == 1:
+                visited[layer][row][col] = True
+                q.append((layer,row,col,0))
+bfs()
+if unripeTomato != 0:
+    print(-1)
+else:
+    print(maxDay)
