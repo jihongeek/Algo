365608b8ec81adc94b008b3ceeef20e2a36dda62
jihongeek <jihongeek@kakao.com>
2021-09-16 12:08:39 +0900

    <not finished!> map :

---
+++ baekjoon/7785.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int logNum;
+    vector <string> nameVector;
+    unordered_map <string,bool> map;
+    cin >> logNum;
+    for (int i = 0; i< logNum; i++)
+    {
+        string name, behaviour;
+        cin >> name >> behaviour;
+    }
+}
