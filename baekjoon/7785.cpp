f40671598fc58a656e61d3cd343ff7b3c9fab011
jihongeek <jihongeek@kakao.com>
2021-09-20 11:53:16 +0900

    ds : using map

--- baekjoon/7785.cpp
+++ baekjoon/7785.cpp
@@ -1,19 +1,44 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
+#include <map>
 #include <algorithm>
 #include <string>
+#include <cstdio>
 using namespace std;
 
 int main()
 {
     int logNum;
-    vector <string> nameVector;
-    unordered_map <string,bool> map;
-    cin >> logNum;
+    map <string,bool> mapOfPeople;
+    map <string,bool> :: iterator iter;
+
+    scanf("%d",&logNum);
+
     for (int i = 0; i< logNum; i++)
     {
+        char tempName[6], tempBehavior[6];
         string name, behaviour;
-        cin >> name >> behaviour;
+        scanf("%s %s",tempName,tempBehavior); 
+        name = tempName;
+        behaviour = tempBehavior;
+        map <string,bool> :: iterator peopleIter =  mapOfPeople.find(name); 
+        if (behaviour == "enter")
+        {
+            if (peopleIter == mapOfPeople.end())
+                mapOfPeople.insert(make_pair(name,true));
+            else
+                peopleIter->second = true;
+
+        }
+        else
+        {
+            mapOfPeople.find(name)->second = false;
+        }
+        
+    }
+    for (iter = --mapOfPeople.end(); iter != --mapOfPeople.begin() ; iter--)
+    {
+            if (iter -> second)
+                printf("%s\n",iter->first.c_str());
     }
 }
