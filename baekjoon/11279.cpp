32dd8908bd1c0836606ae23987eed613a8e1bdb7
jihongkim <jihongeek@kakao.com>
2021-09-27 15:48:39 +0900

    ds : max heap with priority queue

---
+++ baekjoon/11279.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(void)
+{
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    priority_queue <int> heap;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        if (num == 0)
+        {
+            if (heap.empty()) 
+                cout << 0 <<'\n';
+            else
+            {
+                cout << heap.top() << '\n';
+                heap.pop();
+            }
+        }
+        else 
+            heap.push(num);    
+    }
+}
